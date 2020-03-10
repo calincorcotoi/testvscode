59692dd7d782fbc1a56e84ee722946bdd459b57e
Calin Corcotoi <calin1996mh@gmail.com>
2020-03-10 16:47:40 +0200

    remove some code

--- test.py
+++ test.py
@@ -8,11 +8,7 @@ print("This is a test")
 r = requests.get(
     "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjh2rWO5o3oAhUtxosKHdW6AigQPAgH"
 )
+print(r.ok)
 print(r.status_code)
 a = "asdas"
 print("asda")
-
-
-def qwe():
-    n = "asdas"
-    return "asdas"
