bf38d5989e673af2ac0ad04d96f259a3f314f2f3
Corcotoi <corcca1@hro.hella.com>
2020-03-10 16:36:30 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print("This is a test")
+r = requests.get(
+    "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjh2rWO5o3oAhUtxosKHdW6AigQPAgH"
+)
+print(r.status_code)
+a = "asdas"
+print("asda")
+
+
+def qwe():
+    n = "asdas"
+    return "asdas"
