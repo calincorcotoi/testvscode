883c9053b452fa00f30b4411e6bbc7acb8a7c1b3
Calin Corcotoi <calin1996mh@gmail.com>
2020-03-11 08:51:24 +0200

    modificare

--- test.py
+++ test.py
@@ -8,8 +8,7 @@ print("This is a test")
 r = requests.get(
     "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjh2rWO5o3oAhUtxosKHdW6AigQPAgH"
 )
+
 print(r.ok)
 print(r.status_code)
 a = "asdas"
-print("asda")
-print("aSADASD")
